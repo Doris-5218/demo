65029871df6d9c41dce188ff8114319e6fc318a0
Lucien Chung <qpp852@gmail.com>
2022-04-03 16:34:41 +0800

    20220403

---
+++ python-cheat3/3-4.py
@@ -0,0 +1,4 @@
+name=["Lucien","Mia","Doris"]
+print("Hello,"+name[0]+"\nPlease come to my party!")
+print("Hello,"+name[1]+"\nPlease come to my party!")
+print("Hello,"+name[2]+"\nPlease come to my party!")
---
+++ python-cheat3/3-5.py
@@ -0,0 +1,8 @@
+name=["Lucien","Mia","Doris"]
+too_hard="Doris"
+name.remove(too_hard)
+name.append("Ada")
+print("Becase "+too_hard+ " unavable to come, so her brother will come to for her")
+print("Hello,"+name[0]+"\nPlease come to my party!")
+print("Hello,"+name[1]+"\nPlease come to my party!")
+print("Hello,"+name[2]+"\nPlease come to my party!")
